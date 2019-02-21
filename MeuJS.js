139bafb79f403c475529c3bb6d79ee06d5ca74e8
Eder Bevacqua <1801920@SALAS.AULAS>
2019-02-21 11:24:32 -0300

    ..

--- MeuJS.js
+++ MeuJS.js
@@ -1,8 +1,8 @@
 document.getElementById('demo1').onclick=function(){fun1()}
 function fun1(){
     document.getElementById('demo1').innerHTML='OLÁ MUNDÃO';
-    var elem = document.getElementById('DivPrincipal');
-    elem.style.width="100";
+    // var elem = document.getElementById('DivPrincipal');
+    // elem.style.width="100";
 };
 document.getElementById('FunRecarrega').onclick=FunReload;
 function FunReload(){
