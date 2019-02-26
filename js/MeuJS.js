fa520b489a748d1b6da4dbd74d6ee51715efc906
Eder <eder.bevacqua@gmail.com>
2019-02-26 16:15:50 -0300

    front atualizado

--- js/MeuJS.js
+++ js/MeuJS.js
@@ -1,6 +1,6 @@
-document.getElementById('demo1').onclick = function () { fun1() }
+document.getElementById('clickH2').onclick = function () { fun1() }
 function fun1(){
-    document.getElementById('demo1').innerHTML='OLÁ MUNDÃO';
+    document.getElementById('clickH2').innerHTML='OLÁ MUNDÃO';
     // var elem = document.getElementById('DivPrincipal');
     // elem.style.width="100";
 };
