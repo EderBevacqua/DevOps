944f4dadd35d90a460b1a778d2a5900c4c23e781
Eder <eder.bevacqua@gmail.com>
2019-02-26 11:43:03 -0300

    atualização de estrutura

--- js/MeuJS.js
+++ js/MeuJS.js
@@ -1,4 +1,4 @@
-document.getElementById('demo1').onclick=function(){fun1()}
+document.getElementById('demo1').onclick = function () { fun1() }
 function fun1(){
     document.getElementById('demo1').innerHTML='OLÁ MUNDÃO';
     // var elem = document.getElementById('DivPrincipal');
